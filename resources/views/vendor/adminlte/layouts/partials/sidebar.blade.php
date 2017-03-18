92cd5878425c6ece1deaf96e190bd6f4d5edb6de
Maycon Brito <mayconfsbrito@gmail.com>
2017-03-18 00:17:53 -0300

    Defined Menu Options

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -31,17 +31,52 @@
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-            <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
-            <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
-            <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
-                <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                </ul>
+            <li class="header">
+                {{ trans('adminlte_lang::message.header') }}
             </li>
+            <!-- Optionally, you can add icons to the links -->
+            <li class="active">
+                <a href="{{ url('home') }}">
+                    <i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.home') }}</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.centro_custos') }}</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.cotacao') }}</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.obras') }}</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.produtos') }}</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.relatorio_geral') }}</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.relatorio_apartamento') }}</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"><i class='fa fa-link'></i>
+                    <span>{{ trans('adminlte_lang::message.usuarios') }}</span>
+                </a>
+            </li>
+
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
