e7facc84f61ffb8acd23c971522c0165695623a1
Maycon Brito <mayconfsbrito@gmail.com>
2017-04-02 18:47:35 -0300

    Apartament Report Prototype - Created Controller Class and View to PDF

--- app/Http/Controllers/RelatorioApartamento.php
+++ app/Http/Controllers/RelatorioApartamento.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PDF;
 
 class RelatorioApartamento extends Controller
 {
-    //
+    public function index()
+    {
+    	$pdf = PDF::loadView('pdf/relatorio_apartamento');
+    	return $pdf->stream();
+    }
 }
---
+++ resources/views/pdf/relatorio_apartamento.blade.php
@@ -0,0 +1,60 @@
+<head>
+    <meta charset="utf-8">
+    <title>Relatório Sintético</title>
+</head>
+<body>
+    <div align="center">
+        <h3>Relatório Apartamento</h3>
+    </div>
+    <div>
+    <div>
+        <h4>Prédio Residencial X</h4>
+    </div>
+        <table style="width:100%">
+            <thead>
+                <tr>
+                    <th>Produto</th>
+                    <th>Classe</th>
+                    <th>Quantidade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Cano 3/4</td>
+                    <td>Hidráulico</td>
+                    <td>200</td>
+                </tr>
+                <tr>
+                    <td>Joelho 3/4</td>
+                    <td>Hidráulico</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td>Revestimento de Parede Borda Arredondada Brilhante 101A 20x20cm Ceral</td>
+                    <td>Acabamento</td>
+                    <td>1.000</td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <div>
+            <h4>Edifício Protótipo</h4>
+        </div>
+        <table style="width:100%">
+            <thead>
+                <tr>
+                    <th>Produto</th>
+                    <th>Classe</th>
+                    <th>Quantidade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Cimento Itaú Obras Estruturais Saco de 50Kg Votoran</td>
+                    <td>Hidráulico</td>
+                    <td>120</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
