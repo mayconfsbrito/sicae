cea36bbe28e4ca06a48075f216fa4a4265540582
Maycon Brito <mayconfsbrito@gmail.com>
2017-04-02 18:47:54 -0300

    Sintetic Report Prototype - Created Controller Class and View to PDF

--- app/Http/Controllers/RelatorioSintetico.php
+++ app/Http/Controllers/RelatorioSintetico.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PDF;
 
 class RelatorioSintetico extends Controller
 {
-    //
+    public function index()
+    {
+    	$pdf = PDF::loadView('pdf/relatorio_sintetico');
+    	return $pdf->stream();
+    }
 }
---
+++ resources/views/pdf/relatorio_sintetico.blade.php
@@ -0,0 +1,55 @@
+<head>
+    <meta charset="utf-8">
+    <title>Relatório Sintético</title>
+</head>
+<body>
+    <div align="center">
+        <h3>Relatório Sintético</h3>
+        <h4>Período:</h4>01/02/2017 a 15/02/2017
+    </div>
+    <table style="width:100%">
+        <thead>
+            <tr>
+                <th>Produtos</th>
+                <th>Classe</th>
+                <th>Quantidade</th>
+                <th>Preço Médio</th>
+                <th>Valor Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Cano 3/4</td>
+                <td>Hidráulico</td>
+                <td>200</td>
+                <td>5,00</td>
+                <td>1.000,00</td>
+            </tr>
+            <tr>
+                <td>Joelho 3/4</td>
+                <td>Hidráulico</td>
+                <td>100</td>
+                <td>0,50</td>
+                <td>50,00</td>
+            </tr>
+            <tr>
+                <td>Revestimento de Parede Borda Arredondada Brilhante 101A 20x20cm Ceral</td>
+                <td>Acabamento</td>
+                <td>1.000</td>
+                <td>3,00</td>
+                <td>3.000,00</td>
+            </tr>
+            <tr>
+                <td>Cimento Itaú Obras Estruturais Saco de 50Kg Votoran</td>
+                <td>Hidráulico</td>
+                <td>120</td>
+                <td>15,00</td>
+                <td>1.800,00</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <th colspan="4">Valor Total</th>
+            <th>R$ 5.850,00</th>
+        </tfoot>
+    </table>
+</body>
